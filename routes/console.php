584609908e5b65a8baa3ac220df1de7e215e1867
Ariyanto Wibowo <ariyanto@finnet-indonesia.com>
2022-05-03 06:53:59 +0700

    Updates /routes

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Console\Commands\sendMailEveryDay;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Artisan::command('sendmail:day',[sendMailEveryDay::class]);
+
+Artisan::command('sendmail:run',function(){
+    // Artisan::call('sendmail:day');
+    // Artisan::call('sendmail:week');
+    // Artisan::call('sendmail:month');
+    Artisan::queue('sendmail:payment');
+    Artisan::queue('sendmail:createInvoices');
+    Artisan::queue('sendmail:day');
+    Artisan::queue('sendmail:week');
+    Artisan::queue('sendmail:month');
+})->describe('Running commands');
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,30 @@
 <?php
 
+use App\Http\Controllers\AccessMenuController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BlanketController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\ContractPo;
+use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\ContractsController;
+use App\Http\Controllers\EmailConfigController;
+use App\Http\Controllers\EmailsController;
+//use App\Http\Controllers\Contract_docController;
+use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\OperationalsController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\Progress_statusController;
+use App\Http\Controllers\Projects_statusController;
+use App\Http\Controllers\TypeController;
+use App\Http\Controllers\ContractprojectsController;
+use App\Http\Controllers\PayableController;
+use App\Http\Controllers\ProjectCardController;
+use App\Http\Controllers\SuplierController;
+use App\Http\Controllers\TaxController;
+use App\Http\Controllers\TaxProjectCostController;
+use App\Http\Controllers\TaxProofController;
+use App\Http\Controllers\UseblanketController;
+use App\Models\Email;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,45 +36,71 @@ use App\Http\Controllers\ContractsController;
 |
 */
 
+
+
 Route::get('/login',[AuthController::class, 'index'])->name('login');
-Route::post('/login',[AuthController::class, 'callapiusinglaravelui'])->name('loged-in');
+Route::post('/login/api/ui', [AuthController::class, 'callapiusinglaravelui'])->name('loginui');
 Route::get('/logout',[AuthController::class, 'logout'])->name('logout');
 
-
 Route::middleware(['authapi','menu'])->group(function () {
-
-    Route::get('/', function () { return view('home.index'); })->name('Dashboard');
-
-    Route::group(['prefix'=>'operational'], function()
-    {
-        Route::get('/contract-po', [ContractPo::class, 'index'])->name('contract-po');
-
-        Route::resource('/contracts',ContractsController::class);
-        Route::get('/contracts/{contract}/ammend', [ContractsController::class, 'ammend'])->name('contracts.ammend');
-        Route::put('/contracts/{contract}/ammend', [ContractsController::class, 'upammend'])->name('contracts.upammend');
-        Route::post('/contract_doc/{contract_doc}', [ContractsController::class, 'destroyDoc'])->name('contracts.destroyDoc');
-        Route::get('/contracts/history_show/{id}', [ContractsController::class, 'history_show'])->name('contracts.history');
-
-        Route::post('/contract-po/create', function () {
-            return view('contract-po.create');
-        })->name('create.contract');
-
-        Route::get('/operational-cost', function () {
-                return view('operational-cost.index');
-        })->name('operational-cost');
+    Route::get('/', function () {
+        return redirect()->intended('/client');
     });
+    //user
+    Route::get('/user-profile',[AuthController::class, 'userProfile'])->middleware('authapi')->name('profile');
+
+    Route:: view('/operationals','v_operational')->middleware('authapi');
+    Route::resource('/client',ClientsController::class)->middleware('authapi');
+
+    Route::resource('/contractProjects',ContractprojectsController::class)->middleware('authapi');
+    //Route::get('/contractProjects/{contract}/ammend', [ContractprojectsController::class, 'ammend'])->middleware('authapi');
+    //Route::put('/contractProjects/{contract}', [ContractprojectsController::class, 'upammend'])->middleware('authapi');
+    //Route::post('/contractProjects_doc/{contract_doc}', [ContractprojectsController::class, 'destroyDoc'])->middleware('authapi');
+
+    Route::resource('/contracts',ContractsController::class)->middleware('authapi');
+    Route::get('/contracts/{contract}/ammend', [ContractsController::class, 'ammend'])->middleware('authapi');
+    Route::put('/contracts/{contract}', [ContractsController::class, 'upammend'])->middleware('authapi');
+    Route::post('/contract_doc/{contract_doc}', [ContractsController::class, 'destroyDoc'])->middleware('authapi');
+    Route::get('/contracts/history_show/{id}', [ContractsController::class, 'history_show'])->middleware('authapi');
+
+    Route::resource('/projects', ProjectsController::class)->middleware('authapi');
+    Route::get('/projects/{project}/ammend', [ProjectsController::class, 'ammend'])->middleware('authapi');
+    Route::put('/projects/{project}', [ProjectsController::class, 'upammend'])->middleware('authapi');
+    Route::post('/progress_item/{progress_item}', [ProjectsController::class, 'destroyItem'])->middleware('authapi');
+    Route::post('/project_cost/{project_cost}', [ProjectsController::class, 'destroyCost'])->middleware('authapi');
+    Route::get('/projects/history_show/{id}', [ProjectsController::class, 'history_show'])->middleware('authapi');
+
+    Route::resource('/operationals', OperationalsController::class)->middleware('authapi');
+    Route::post('/progress_doc', [OperationalsController::class, 'uploadProgress'])->middleware('authapi');
+    Route::get('/changestatus/{changestatus}', [OperationalsController::class, 'changeStatus'])->middleware('authapi');
+    Route::get('/progress_doc/{progress_doc}', [OperationalsController::class, 'destroyDoc'])->middleware('authapi');
+
+    Route::resource('/projects_status', Projects_statusController::class)->middleware('authapi');
+
+    Route::resource('/payments', PaymentController::class)->middleware('authapi');
+    Route::resource('/blanket', BlanketController::class)->middleware('authapi');
+    Route::resource('/useblanket', UseblanketController::class)->middleware('authapi');
+
+    Route::resource('/taxproof', TaxProofController::class)->middleware('authapi');
+    Route::get('/applytax/{id}', [TaxProofController::class, 'applytax'])->middleware('authapi');
+
+    Route::resource('/taxprojectcost', TaxProjectCostController::class)->middleware('authapi');
+
+    Route::resource('/suplier', SuplierController::class)->middleware('authapi');
+
+    Route::resource('/projectcard', ProjectCardController::class)->middleware('authapi');
+    Route::get('/projectcard/export/excel/{id}', [ProjectCardController::class, 'export'])->middleware('authapi');
+
+    Route::resource('/payable', PayableController::class)->middleware('authapi');
+    Route::get('/payable/{id}/bill', [PayableController::class,'pay'])->middleware('authapi');
 });
 
-
-Route::name('Monitoring')->group( function () {
-    Route::get('/project-card', function () {
-        return view('project-card.index');
-    })->name('project-card');
+//config
+Route::middleware(['authapi','admin'])->group(function () {
+    Route::resource('/progress_status', Progress_statusController::class)->middleware('authapi');
+    Route::resource('/access_menu', AccessMenuController::class)->middleware('authapi');
+    Route::resource('/email', EmailsController::class)->middleware('authapi');
+    Route::resource('/email_configuration', EmailConfigController::class)->middleware('authapi');
+    Route::resource('/types', TypeController::class)->middleware('authapi');
+    Route::get('/send-mail',[EmailsController::class,'sendMail']);
 });
-
-Route::name('Settings')->group( function () {
-    Route::get('/master-clients', function () {
-        return view('master-clients.index');
-    })->name('master-clients');
-});
-
